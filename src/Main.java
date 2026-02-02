1daab62b24337d80ddeab4cfd84f55de7dd9edf3
Miko Berulava <veorna7@gmail.com>
2026-02-02 11:48:55 +0400

    add calculator logic

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.print("I Like Pizza\n");
+        System.out.print("Buy Me Pizza");
+        System.out.println("First Java Proggram");
+    }
+}
